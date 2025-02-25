ef917f23a4f62ad20a642c01b6cdf201034a3520
LGugs3 <liam.gugliotta@mymail.champlain.edu>
2025-02-25 16:51:12 -0500

    added init.py

---
+++ DBStuff/__init__.py
@@ -0,0 +1,7 @@
+"""Docstring."""
+from sakilaConnection import MySQLDatabase
+import queries
+
+__all__ = ["MySQLDatabase", "MySQLQuery", "ALL_QUERIES", "get_query_by_name"]
+__version__ = "0.0.1"
+__author__ = "Liam Gugliotta, Adrian Bassir"
